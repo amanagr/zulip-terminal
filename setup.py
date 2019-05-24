2be614ddcd356f027c34a2135405c3defce67e67
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2019-05-24 06:58:44 +0530

    run: Add UploadCommand class to automate release process.
    Moved this code form https://github.com/zulip/zulint/blob/master/setup.py.

--- setup.py
+++ setup.py
@@ -44,6 +44,45 @@ dev_helper_deps = [
     'gitlint>=0.10',
 ]
 
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(
+                                                            sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
 setup(
     name='zulip-term',
     version=ZT_VERSION,
@@ -93,4 +132,8 @@ setup(
         'lxml==4.2.3',
         'mypy_extensions>=0.4',
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
